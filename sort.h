1fb61b8f1d5f938d6495cc7a47cbd21b1c6226ba
Otung Princess <otungprincess@gmail.com>
2022-07-19 15:45:11 -0700

    algorithms

--- sort.h
+++ sort.h
@@ -30,6 +30,7 @@ void swap_ints(int *a, int *b);
 int lomuto_partition(int *array, size_t size, int left, int right);
 void lomuto_sort(int *array, size_t size, int left, int right);
 void shell_sort(int *array, size_t size);
+size_t list_len(listint_t *list);
 void switch_nodes(listint_t **list, listint_t **p);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
@@ -38,12 +39,14 @@ void merge_recursion(int *arr, int *array, size_t left, size_t right);
 void merge_subarray(int *arr, int *array, size_t left,
 		size_t middle, size_t right);
 void heap_sort(int *array, size_t size);
-void heapify(int *array, size_t s, size_t root, size_t size);
+void max_heapify(int *array, size_t s, size_t root, size_t size);
 void radix_sort(int *array, size_t size);
 void count_sort_LSD(int *array, size_t size, size_t lsd);
-void quick_sort_hoare(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void bitonic_recursion(int *array, int l, int r, int direction, size_t size);
 void bitonic_merge(int *array, int l, int r, int direction);
+void quick_sort_hoare(int *array, size_t size);
+void quick_recursion(int *array, int left, int right, size_t size);
+int partition(int *array, int left, int right, size_t size);
 
 #endif
